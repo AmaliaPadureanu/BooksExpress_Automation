ed2b4f97040c0903eec04fa3040e8c1a84824375
AmaliaPadureanu <79747055+AmaliaPadureanu@users.noreply.github.com>
2022-11-19 17:15:49 +0200

    created WaitPageUtils for page classes to store generic wait methods and replaced the waits from ContactPage methods with a wait method called from WaitPageUtils

--- src/main/java/Pages/ContactPage.java
+++ src/main/java/Pages/ContactPage.java
@@ -1,12 +1,11 @@
 package Pages;
 
+import Utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ContactPage {
 
@@ -16,35 +15,24 @@ public class ContactPage {
         this.driver = driver;
     }
 
-    private String SUBJECT_DROPDOWN = "//select[@id='subject']";
-    private String MESSAGE_FIELD = "//textarea[@id='message']";
-    private String ORDER_NO_FIELD = "//input[@id='order_number']";
-    private String NAME_FIELD = "//input[@id='name']";
-    private String EMAIL_FIELD = "//input[@id='email']";
-    private String SEND_BTN = "//a[@id='send-message']";
-
     public void sendContactFormUnregisteredUser(String subject, String message, String name, String email, String orderNo) {
-        driver.findElement(By.xpath(SUBJECT_DROPDOWN)).click();
-        Select select = new Select(driver.findElement(By.xpath(SUBJECT_DROPDOWN)));
+        Select select = new Select(driver.findElement(By.id("subject")));
         select.selectByVisibleText(subject);
-        driver.findElement(By.xpath(MESSAGE_FIELD)).sendKeys(message);
-        driver.findElement(By.xpath(NAME_FIELD)).sendKeys(name);
-        driver.findElement(By.xpath(EMAIL_FIELD)).sendKeys(email);
-        driver.findElement(By.xpath(ORDER_NO_FIELD)).sendKeys(orderNo);
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(SEND_BTN))));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].click();", element);
+        driver.findElement(By.id("message")).sendKeys(message);
+        driver.findElement(By.id("name")).sendKeys(name);
+        driver.findElement(By.id("email")).sendKeys(email);
+        driver.findElement(By.id("order_number")).sendKeys(orderNo);
+        WebElement sendBtn = WaitUtils.waitForElementToBeClickable(driver, By.id("send-message"), 5);
+        ((JavascriptExecutor)driver).executeScript("arguments[0].click();", sendBtn);
     }
 
     public void sendContactFormRegisteredUser(String subject, String message, String orderNo) {
-        driver.findElement(By.xpath(SUBJECT_DROPDOWN)).click();
-        Select select = new Select(driver.findElement(By.xpath(SUBJECT_DROPDOWN)));
+        Select select = new Select(driver.findElement(By.id("subject")));
         select.selectByVisibleText(subject);
-        driver.findElement(By.xpath(MESSAGE_FIELD)).sendKeys(message);
-        driver.findElement(By.xpath(ORDER_NO_FIELD)).sendKeys(orderNo);
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath(SEND_BTN))));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].click();", element);
+        driver.findElement(By.id("message")).sendKeys(message);
+        driver.findElement(By.id("order_number")).sendKeys(orderNo);
+        WebElement sendBtn = WaitUtils.waitForElementToBeClickable(driver, By.id("send-message"), 5);
+        ((JavascriptExecutor)driver).executeScript("arguments[0].click();", sendBtn);
     }
 
 }
