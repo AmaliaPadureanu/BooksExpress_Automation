e71ed81743f6e497c504cc73f606b4f077b7b7c9
AmaliaPadureanu <79747055+AmaliaPadureanu@users.noreply.github.com>
2022-12-02 10:45:33 +0200

    refactored NavigationPage using DSL

--- src/main/java/pages/NavigationPage.java
+++ src/main/java/pages/NavigationPage.java
@@ -1,112 +1,111 @@
 package pages;
 
-import utils.WaitUtils;
+import base.BasePage;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import utils.JavaScriptUtils;
+import utils.WaitUtils;
 
-public class NavigationPage {
+public class NavigationPage extends BasePage {
 
-    WebDriver driver;
+    private By SALES_LINK = By.xpath("//a[@href='/reduceri']");
+    private By TOP_SALES_LINK = By.xpath("//a[@href='/top/carti']");
+    private By NEW_PRODUCTS_LINK = By.xpath("//div[@id='submenu']//a[contains(text(),'Noutăți')]");
+    private By GIFTS_LINK = By.xpath("//a[normalize-space()='Cadouri']");
+    private By BLOG_LINK = By.xpath("//div[@id='submenu']//a[normalize-space()='Blog']");
+    private By NEWSLETTER_LINK = By.xpath("//a[contains(text(),'Abonează-te la newsletter!')]");
+    private By CONTACT_LINK = By.xpath("//a[normalize-space()='Contact']");
+    private By PRODUCTS_LINK = By.id("products");
+    private By BUSINESS_CATEGORY = By.xpath("//a[@class='category-menu'][normalize-space()='Business']");
+    private By ACCOUNT_LINK = By.xpath("//a[@id='show-user']//i[@class='fa fa-angle-down']");
+    private By PERSONAL_INFO = By.xpath("//ul[@class='jq-dropdown-menu']//a[normalize-space()='Detalii personale']");
+    private By NAVIGATION_HISTORY = By.xpath("//a[normalize-space()='Istoric de navigare']");
+    private By LISTS = By.id("show-lists");
+
+    private By SEE_ALL_LISTS = By.linkText("Toate listele");
+    private By WISHLIST_LINK = By.cssSelector("a[href='/user/wishlist']");
+    private By SEE_CART_BTN = By.xpath("//a[contains(text(),'Vezi coșul')]");
+    private By CART_BTN = By.xpath("//span[contains(text(),'Coș')]");
 
     public NavigationPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
-    private String REDUCERI_LINK = "//a[@href='/reduceri']";
-    private String TOP_VANZARI_LINK = "//a[@href='/top/carti']";
-    private String NOUTATI_LINK = "//div[@id='submenu']//a[contains(text(),'Noutăți')]";
-    private String CADOURI_LINK = "//a[normalize-space()='Cadouri']";
-    private String BLOG_LINK = "//div[@id='submenu']//a[normalize-space()='Blog']";
-    private String NEWSLETTER_LINK = "//a[contains(text(),'Abonează-te la newsletter!')]";
-    private String CONTACT_LINK = "//a[normalize-space()='Contact']";
-    private String PRODUCTS_LINK = "//a[@id='products']";
-    private String BUSINESS_CATEGORY = "//a[@class='category-menu'][normalize-space()='Business']";
-    private String CONT_LINK = "//a[@id='show-user']//i[@class='fa fa-angle-down']";
-    private String PERSONAL_INFO = "//ul[@class='jq-dropdown-menu']//a[normalize-space()='Detalii personale']";
-    private String NAVIGATION_HISTORY = "//a[normalize-space()='Istoric de navigare']";
-    private String LISTS = "//span[normalize-space()='Liste']";
-    private String WISHLIST_BTN = "//a[@href='/user/wishlist']";
-    private String WISHLIST_LINK = "//a[@id='show-lists']//i[@class='fa fa-angle-down']";
-    private String VEZI_COSUL_BTN = "//a[contains(text(),'Vezi coșul')]";
-    private String COS_BTN = "//span[contains(text(),'Coș')]";
-
     public String selectProductsCategory() {
-        Actions actions = new Actions(driver);
+        Actions actions = new Actions(getDriver());
 
-        WebElement products = driver.findElement(By.xpath(PRODUCTS_LINK));
-        WebElement business = driver.findElement(By.xpath(BUSINESS_CATEGORY));
+        WebElement products = find(PRODUCTS_LINK);
+        WebElement business = find(BUSINESS_CATEGORY);
 
         actions.moveToElement(products).moveToElement(business).perform();
-        WaitUtils.waitForElementToBeClickable(driver, By.partialLinkText("Economie"), 5);
-        JavascriptExecutor executor = (JavascriptExecutor)driver;
-        executor.executeScript("arguments[0].click();", driver.findElement(By.partialLinkText("Economie")));
+        WaitUtils.waitForElementToBeClickable(getDriver(), By.partialLinkText("Economie"), 5);
+        JavaScriptUtils.click(getDriver(), find(By.partialLinkText("Economie")));
 
-        return driver.getTitle().toLowerCase();
+        return getDriver().getTitle().toLowerCase();
     }
 
     public void navigateToSales() {
-        driver.findElement(By.xpath(REDUCERI_LINK)).click();
+        find(SALES_LINK).click();
     }
 
     public void navigateToTopSales() {
-        driver.findElement(By.xpath(TOP_VANZARI_LINK)).click();
+        find(TOP_SALES_LINK).click();
     }
 
     public void navigateToNovelties() {
-        driver.findElement(By.xpath(NOUTATI_LINK)).click();
+        find(NEW_PRODUCTS_LINK).click();
     }
 
     public void navigateToGifts() {
-        driver.findElement(By.xpath(CADOURI_LINK)).click();
+        find(GIFTS_LINK).click();
     }
 
     public void blog() {
-        driver.findElement(By.xpath(BLOG_LINK)).click();
+        find(BLOG_LINK).click();
     }
 
     public NewsletterPage navigateToNewsletter() {
-        driver.findElement(By.xpath(NEWSLETTER_LINK)).click();
-        return new NewsletterPage(driver);
+        find(NEWSLETTER_LINK).click();
+        return new NewsletterPage(getDriver());
     }
 
     public ContactPage navigateToContact() {
-        driver.findElement(By.xpath(CONTACT_LINK)).click();
-        return new ContactPage(driver);
+        find(CONTACT_LINK).click();
+        return new ContactPage(getDriver());
     }
 
     public UserDetailsPage navigateToUserDetails() {
-        Actions actions = new Actions(driver);
-        actions.moveToElement(driver.findElement(By.xpath(CONT_LINK))).click(driver.findElement(By.xpath(PERSONAL_INFO))).perform();
-        return new UserDetailsPage(driver);
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(find(ACCOUNT_LINK)).click(find(PERSONAL_INFO)).perform();
+        return new UserDetailsPage(getDriver());
     }
 
     public NavigationHistoryPage navigateToUserNavigationHistory() {
-        Actions actions = new Actions(driver);
-        actions.moveToElement(driver.findElement(By.xpath(CONT_LINK))).click(driver.findElement(By.xpath(NAVIGATION_HISTORY))).perform();
-        return new NavigationHistoryPage(driver);
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(find(ACCOUNT_LINK)).click(find(NAVIGATION_HISTORY)).perform();
+        return new NavigationHistoryPage(getDriver());
     }
 
     public WishlistPage navigateToWishlist() {
-        Actions actions = new Actions(driver);
-        actions.moveToElement(driver.findElement(By.xpath("//a[@id='show-lists']//i[@class='fa fa-angle-down']")))
-                .click(driver.findElement(By.xpath("//a[@href='/user/wishlist']//i[@class='fa fa-heart']"))).perform();
-        return new WishlistPage(driver);
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(find(LISTS))
+                .click(find(WISHLIST_LINK)).perform();
+        return new WishlistPage(getDriver());
     }
 
     public ShoppingCartPage navigateToCart() {
-        driver.findElement(By.xpath(COS_BTN)).click();
-        WaitUtils.waitForElementToBeClickable(driver, By.xpath(VEZI_COSUL_BTN), 5).click();
-        return new ShoppingCartPage(driver);
+        find(CART_BTN).click();
+        WaitUtils.waitForElementToBeClickable(getDriver(), SEE_CART_BTN, 5).click();
+        return new ShoppingCartPage(getDriver());
     }
 
     public ListsPage navigateToLists() {
-        Actions actions = new Actions(driver);
-        actions.moveToElement(driver.findElement(By.id("show-lists"))).perform();
-        driver.findElement(By.linkText("Toate listele")).click();
-        return new ListsPage(driver);
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(find(LISTS)).perform();
+        find(SEE_ALL_LISTS).click();
+        return new ListsPage(getDriver());
     }
 
 }
